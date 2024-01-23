b6f3557ff55dfc2600a19cf6e6d0044550f10530
SeYong <jkjk368@naver.com>
2024-01-23 17:13:07 +0900

    초기 생성

---
+++ src/main/java/project3/newfoodfinder/NewfoodfinderApplication.java
@@ -0,0 +1,13 @@
+package project3.newfoodfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewfoodfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewfoodfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project3/newfoodfinder/NewfoodfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package project3.newfoodfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewfoodfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
