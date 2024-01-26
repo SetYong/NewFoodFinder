70c65480161424b0dbc1ecabc9dcfe4de001fb90
eune2507 <uforu3@gmail.com>
2024-01-26 17:38:35 +0900

    1/26 17:38 회원가입 DB 완료 / 로그인 작업중

---
+++ src/main/java/project3/newfoodfinder/controller/NewfoodfinderController.java
@@ -0,0 +1,53 @@
+package project3.newfoodfinder.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import project3.newfoodfinder.dto.MemberDTO;
+import project3.newfoodfinder.service.MemberService;
+
+@Controller
+@RequestMapping("/newfoodfinder")
+@Log4j2
+@RequiredArgsConstructor
+public class NewfoodfinderController {
+
+    private final MemberService service;
+
+    @GetMapping({"/","/home"})
+    public String home(){
+        log.info("home...");
+        return "/newfoodfinder/home";
+    }
+
+    // get 방식으로 회원가입 화면보여주기
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get...");
+    }
+    // post 방식으로 회원가입 등록처리
+    @PostMapping("/register")
+    public String registerPost(MemberDTO dto, RedirectAttributes redirectAttributes){
+        log.info("dto....");
+        Long mbnum = service.register(dto);
+        redirectAttributes.addFlashAttribute("msg", mbnum);
+        return "redirect:/newfoodfinder/home";
+    }
+
+    //get 방식으로 로그인 화면 보여주기
+    @GetMapping("/login")
+    public void login(){
+        log.info("login get...");
+    }
+    //post 방식으로 로그인 확인하기
+    @PostMapping("/login")
+    public boolean loginPost(MemberDTO dto, RedirectAttributes redirectAttributes){
+        log.info("login...");
+        boolean pass = true;
+        return pass;
+    }
+}
---
+++ src/main/java/project3/newfoodfinder/service/MemberServiceImpl.java
@@ -0,0 +1,36 @@
+package project3.newfoodfinder.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import project3.newfoodfinder.dto.MemberDTO;
+import project3.newfoodfinder.entity.Member;
+import project3.newfoodfinder.repository.MemberRepository;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository repository;
+
+    @Override
+    public Long register(MemberDTO dto){
+        log.info("DTO----------------------------");
+        log.info(dto);
+
+        Member entity = dtoToEntity(dto);
+        log.info(entity);
+
+        repository.save(entity);
+        return entity.getMbnum();
+    }
+
+    @Override
+    public boolean login(MemberDTO dto){
+        log.info("login dto----------------------");
+        log.info(dto);
+        boolean pass = true;
+        return pass;
+    }
+}
