4655f16f35ed84e3bc330557e972e9bf0ead0b36
eune2507 <uforu3@gmail.com>
2024-02-05 17:49:25 +0900

    2/5 17:49 spring security 진행중

--- src/main/java/project3/newfoodfinder/config/SecurityConfig.java
+++ src/main/java/project3/newfoodfinder/config/SecurityConfig.java
@@ -1,24 +1,22 @@
 package project3.newfoodfinder.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import project3.newfoodfinder.service.MemberService;
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig extends WebSecurityConfiguration {
+public class SecurityConfig {
 
 
-    protected void configure(HttpSecurity http) throws Exception {
-
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
 }
